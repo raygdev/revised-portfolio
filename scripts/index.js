1ea3e8e45ab64593339b477ec6820542250d621e
raygdev <101662915+raygdev@users.noreply.github.com>
2022-09-07 23:26:01 -0400

    Add check for length on visibility
    changed if condition to add forgotten check
    for length of visibility string

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector(".nav");
 let sectionsList = Array.from(document.querySelectorAll(".section"));
 
 menubtn.addEventListener("click", function () {
-  if (navbar.style.visibility === 0) {
+  if (navbar.style.visibility.length === 0) {
     navbar.style.visibility = "visible";
   } else {
     navbar.style.visibility = "";
