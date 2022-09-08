e94ba2bf886d166b3b70e2fd4bfa4fd1d74d13c9
raygdev <101662915+raygdev@users.noreply.github.com>
2022-09-07 22:07:03 -0400

    Add base functionality for Animation scroll
    set up initial functionality for animation scrolling
    and will add css selectors and key frames later

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const menubtn = document.querySelector("#menu-btn");
 const navbar = document.querySelector(".nav");
-let sectionsList = Array.from(document.querySelectorAll(".section"));
+let sectionsList = Array.from(document.querySelectorAll(".collaboration"));
 
 menubtn.addEventListener("click", function () {
   if (navbar.style.visibility === "hidden") {
@@ -9,3 +9,22 @@ menubtn.addEventListener("click", function () {
     navbar.style.visibility = "hidden";
   }
 });
+
+const isElementInView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+        elementTop <= (window.innerHeight || document.documentElement.clientHeight)
+    )
+}
+
+window.addEventListener('scroll', function() {
+    sectionsList.forEach(element => {
+        if(isElementInView(element)){
+            setTimeout(()=> {
+                element.style.visibility = 'visible'
+            },500)
+        }else {
+            element.style.visibility = 'hidden'
+        }
+    });
+})
